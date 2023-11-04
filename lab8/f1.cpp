2573845c19ba61fe361b827e8251a0aacdd38bd1
Nelle <nurievaaa28@gmail.com>
2023-11-04 22:51:20 +0600

    init commit

---
+++ lab8/f1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+long long MOD = 1e15 + 9;
+long long X = 31;
+
+long long otvet(string str){
+    unordered_map <long long, bool> hashes;
+    for (long long i = 0; i < str.size(); i++) {
+        long long d = 0;
+        for (long long j = i; j < str.size(); j++) {
+            d = (X * d + str[j]) % MOD;
+
+            if (hashes.find(d) == hashes.end()) {
+                hashes[d] = true;
+            }
+        }
+    }
+    return hashes.size();
+}
+
+
+int main() {
+    string s;
+    cin >> s;
+    cout << otvet(s);
+}
