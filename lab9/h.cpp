56f4f0adca14ab9ac055d17cd96874c0fe7f85c9
Nelle <nurievaaa28@gmail.com>
2023-11-07 18:38:47 +0600

    init commit

--- lab9/h.cpp
+++ lab9/h.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> z_fun(string s) {
+vector<int> z_function(string s) {
     vector<int> z(s.size());
+    
     for (int i = 1, l = 0, r = 0; i < s.size(); i++) {
         if (i <= r) z[i] = min(z[i - l], r - i + 1);
         while (i + z[i] < s.size()) {
@@ -18,7 +19,7 @@ vector<int> z_fun(string s) {
 int main() {
     string s;
     cin >> s;
-    vector<int> v = z_fun(s);
+    vector<int> v = z_function(s);
 
     for(int i = 0; i < v.size(); i++){
         cout << v[i] << ' ';
