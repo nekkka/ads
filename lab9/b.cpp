4e6f06eff7bb2c30bf130fdd685470d05ae89605
Nelle <nurievaaa28@gmail.com>
2023-11-10 14:48:15 +0600

    init commit

---
+++ lab9/b.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void computeLPSArray(string pat, int M, int* lps);
+
+vector<int> KMPSearch(string pat, string txt)
+{
+    vector<int> ans;
+    int M = pat.size();
+    int N = txt.size();
+
+    int lps[M];
+
+    computeLPSArray(pat, M, lps);
+
+    int i = 0;
+    int j = 0;
+    while ((N - i) >= (M - j)) {
+        if (pat[j] == txt[i]) {
+            j++;
+            i++;
+        }
+
+        if (j == M) {
+            ans.push_back(i - j + 1);
+            j = lps[j - 1];
+        }
+
+
+        else if (i < N && pat[j] != txt[i]) {
+
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i = i + 1;
+        }
+    }
+    return ans;
+}
+
+void computeLPSArray(string pat, int M, int* lps)
+{
+
+    int len = 0;
+
+    lps[0] = 0;
+
+    int i = 1;
+    while (i < M) {
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else
+        {
+            if (len != 0) {
+                len = lps[len - 1];
+
+            }
+            else
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+int main() {
+    string pat;
+    string text;
+
+    cin >> pat;
+    int kolichestvo; cin >> kolichestvo;
+    cin >> text;
+
+    vector <int> ans = KMPSearch(pat, text);
+
+    if (kolichestvo > ans.size()) {
+        cout << "NO" << endl;
+    }
+    else cout << "YES" << endl;
+}
