0bcea93330bc6fe9703a1ebf83d94083bc99ed49
Nelle <nurievaaa28@gmail.com>
2023-11-15 12:50:08 +0600

    init commit

--- lab10/a.cpp
+++ lab10/a.cpp
@@ -3,31 +3,31 @@ using namespace std;
 int n, m;
 int arr[1003][1003];
 int grib = 0;
-queue<pair<int, int> > q;
+queue<pair<int, int> > que;
 void eat(int i, int j){
     if(i - 1 >= 0 && arr[i-1][j] == 1){
         grib--;
         arr[i-1][j] = 2;
         arr[i][j] = 0;
-        q.push(make_pair(i - 1, j));
+        que.push(make_pair(i - 1, j));
     }
     if(i + 1 < n && arr[i + 1][j] == 1){
         grib--;
         arr[i+1][j] = 2;
         arr[i][j] = 0;
-        q.push(make_pair(i + 1, j));
+        que.push(make_pair(i + 1, j));
     }
     if(j - 1 >= 0 && arr[i][j - 1] == 1){
         grib--;
         arr[i][j - 1] = 2;
         arr[i][j] = 0;
-        q.push(make_pair(i, j - 1));
+        que.push(make_pair(i, j - 1));
     }
     if(j + 1 < m && arr[i][j + 1] == 1){
         grib--;
         arr[i][j + 1] = 2;
         arr[i][j] = 0;
-        q.push(make_pair(i, j + 1));
+        que.push(make_pair(i, j + 1));
     }
     
 }
@@ -38,17 +38,17 @@ int main() {
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
             cin >> arr[i][j];
-            if(arr[i][j] == 2)q.push(make_pair(i, j));
+            if(arr[i][j] == 2)que.push(make_pair(i, j));
             if(arr[i][j] == 1)grib++;
         }
     }
 
-    while(!q.empty()){
+    while(!que.empty()){
         if(grib == 0)break;
-        int sz = q.size();
+        int sz = que.size();
         while(sz--){
-            pair<int, int> pos = q.front();
-            q.pop();
+            pair<int, int> pos = que.front();
+            que.pop();
             eat(pos.first, pos.second);
         }
         res++;
